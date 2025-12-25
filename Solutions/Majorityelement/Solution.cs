72b66933fe5438e458a1d6845a679736099a26a4
vadimkholodilo <vadimkholodilo@gmail.com>
2025-12-26 00:06:11 +0300

    Implement solution with O(n) extra space

---
+++ Solutions/Majorityelement/Solution.cs
@@ -0,0 +1,34 @@
+namespace Solutions.Majorityelement;
+
+public class Solution
+{
+    public int MajorityElement(int[] nums)
+    {
+        if (nums.Length == 1)
+            return nums[0];
+
+        int k = nums.Length / 2;
+        var freeqTable = new Dictionary<int, int>();
+
+        foreach (var num in nums)
+        {
+            if (freeqTable.ContainsKey(num))
+                freeqTable[num] += 1;
+            else
+                freeqTable[num] = 1;
+        }
+
+        foreach ((var key, var value) in freeqTable)
+        {
+            if (value > k)
+                return key;
+        }
+
+        return -1;
+    }
+
+    public int MajorityElementWithoutHashTable(int[] nums)
+    {
+        return 0;
+    }
+}
---
+++ Tests/MajorityelementTests.cs
@@ -0,0 +1,18 @@
+using Xunit;
+using Solutions.Majorityelement;
+
+namespace Tests;
+
+public class MajorityelementTests
+{
+    [Theory]
+    [InlineData(new [] { 1 }, 1)]
+    [InlineData(new [] { 3, 2, 3 }, 3)]
+    [InlineData(new [] {2, 2, 1, 1, 1, 2, 2}, 2)]
+    public void TestSolution(int[] nums, int majorityElement)
+    {
+        var solution = new Solution();
+        Assert.Equal(majorityElement, solution.MajorityElement(nums));
+        //Assert.Equal(majorityElement, solution.MajorityElementWithoutHashTable(nums));
+    }
+}
