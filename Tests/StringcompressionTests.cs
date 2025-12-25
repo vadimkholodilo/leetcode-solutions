c71468bd2b8d019a2b491f9c7e26783f1ea4a4bb
vadimkholodilo <vadimkholodilo@gmail.com>
2025-12-25 22:50:23 +0300

    String compression problem solution

---
+++ Tests/StringcompressionTests.cs
@@ -0,0 +1,23 @@
+using System.Runtime.CompilerServices;
+using Xunit;
+using Solutions.Stringcompression;
+
+namespace Tests;
+
+public class StringcompressionTests
+{
+    [Theory]
+    [InlineData("aabbccc", 6, "a2b2c3")]
+    [InlineData("a", 1, "a")]
+    [InlineData("abbbbbbbbbbbb", 4, "ab12")]
+    public void TestSolution(string input, int expectedLength, string expectedOutput)
+    {
+        var solution = new Solution();
+        var inputArray = input.ToCharArray();
+
+        var actualLength = solution.Compress(inputArray);
+
+        Assert.Equal(expectedLength, actualLength);
+        Assert.Equal(expectedOutput.ToCharArray(), inputArray[..expectedLength]);
+    }
+}
