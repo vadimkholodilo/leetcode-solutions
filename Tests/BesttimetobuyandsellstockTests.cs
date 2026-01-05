477d1be668e5fe6fd2392e315652466e56074628
VadimKholodilo <vadimkholodilo@gmail.com>
2026-01-05 23:20:08 +0300

    Solution: Best Time To Buy And Sell Stock (#4)

---
+++ Tests/BesttimetobuyandsellstockTests.cs
@@ -0,0 +1,17 @@
+using Xunit;
+using Solutions.Besttimetobuyandsellstock;
+
+namespace Tests;
+
+public class BesttimetobuyandsellstockTests
+{
+    [Theory]
+    [InlineData(new int[] {}, 0)]
+    [InlineData(new [] {7,1,5,3,6,4}, 5)]
+    [InlineData(new [] {7,6,4,3,1}, 0)]
+    public void TestSolution(int[] prices, int expectedMaxProfit)
+    {
+        var solution = new Solution();
+        Assert.Equal(expectedMaxProfit, solution.MaxProfit(prices));
+    }
+}
