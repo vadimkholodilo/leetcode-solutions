9fd33aa52e306e3611561041129da30d6efce39d
vadimkholodilo <vadimkholodilo@gmail.com>
2026-01-12 08:05:23 +0300

    Solution: Minimum Sum Subarray

---
+++ Tests/MinimumsizesubarraysumTests.cs
@@ -0,0 +1,17 @@
+using Xunit;
+using Solutions.Minimumsizesubarraysum;
+
+namespace Tests;
+
+public class MinimumsizesubarraysumTests
+{
+    [Theory]
+    [InlineData(new [] {2,3,1,2,4,3}, 7, 2)]
+    [InlineData(new [] {1,4,4}, 4, 1)]
+    [InlineData(new [] {1,1,1,1,1,1,1,1}, 11, 0)]
+    public void TestSolution(int[] nums, int target, int expectedLength)
+    {
+        var solution = new Solution();
+        Assert.Equal(expectedLength, solution.MinSubarrayLen(target, nums));
+    }
+}
